c16d2109d2d8f469d3f0290a41be3b518c8f613e
Masood Ahmad <masahmad7@gmail.com>
2018-04-08 23:25:56 +0100

    add utility files for fetch api

--- src/utility/http.js
+++ src/utility/http.js
@@ -1,16 +1,7 @@
-/**
- * High-level wrapper for the Fetch API (see this post for explanation - https://medium.com/@shahata/why-i-wont-be-using-fetch-api-in-my-apps-6900e6c6fe78#.o6k6ai5fj)
- *
- */
-
-
 import request from './request';
 
 export default class http {
-
-    
-/* @returns {Promise} which returns the response body in JSON format */
-     
+   
 	static get(uri) {
 		return request('GET', uri);
 	}
